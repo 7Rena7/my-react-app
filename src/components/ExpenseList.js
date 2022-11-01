ba5a80be0ed339c014eac875a334bba4a844ed72
Renato Corbellini <renacorbe@gmail.com>
2022-11-01 15:33:31 -0300

    Adding and using Card component

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,13 +1,14 @@
+import Card from "./Card";
 import ExpenseItem from "./ExpenseItem";
 import "./ExpenseList.css";
 
 export default function ExpenseList(props) {
   const { expenses } = props;
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem expense={expenses[0]} />
       <ExpenseItem expense={expenses[1]} />
       <ExpenseItem expense={expenses[2]} />
-    </div>
+    </Card>
   );
 }
